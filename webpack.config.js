4f4fac46b07feb11710b80d02aa9040a7ba9c8bc
Muhammadamin Zakir <perseusp3@gmail.com>
2021-10-25 20:48:10 +0300

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+// var webpack = require('webpack');
+module.exports = {
+  entry: ["@babel/polyfill", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, 'bundles'),
+    filename: 'bundle.js'
+  },
+  // mode: "development",
+  mode: "production",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+  // devServer: {
+  //   port: 3200,
+  //   index: "index.html"
+  // }
+};
